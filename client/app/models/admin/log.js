8c31c9c594c3e590f3e772597b83558947841274
Bryan Hunt <bhunt@mac.com>
2015-06-21 21:11:24 -0500

    Better log formatting

--- client/app/models/admin/log.js
+++ client/app/models/admin/log.js
@@ -1,5 +1,7 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
+var computed = Ember.computed;
 var attr = DS.attr;
 var Model = DS.Model;
 
@@ -7,5 +9,25 @@ export default Model.extend({
   level : attr("Number"),
   bsn : attr("String"),
   time : attr("MongoDate"),
-  message : attr("String")
+  message : attr("String"),
+
+  date : computed('time', function() {
+    var time = this.get('time');
+    return (time.getMonth() + 1) + "-" + time.getDate() + "-" + time.getFullYear();
+  }),
+
+  levelLabel : computed('level', function() {
+    switch (this.get('level')) {
+      case 1:
+        return "ERROR";
+      case 2:
+        return "WARNING";
+      case 3:
+        return "INFO";
+      case 4:
+        return "DEBUG";
+      default:
+        return "UNK";
+    }
+  })
 });
